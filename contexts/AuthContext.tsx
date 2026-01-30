cef45f0fcef1df75eb24ceb58d5f3a0c1cd3439c
tharusenevirathne <tharusanduni33@gmail.com>
2026-01-30 19:33:40 +0530

    add authService

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -24,7 +24,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       hideLoader()
     })
 
-    // cleanup function (component unmount)
     return () => unsucribe()
   }, [])
 

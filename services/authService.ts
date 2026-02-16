43456acde4313bd98b6af2b6e075c5b1833293f5
tharusenevirathne <tharusanduni33@gmail.com>
2026-02-16 14:19:45 +0530

    add new feature

--- services/authService.ts
+++ services/authService.ts
@@ -2,6 +2,11 @@ import { createUserWithEmailAndPassword,updateProfile,signInWithEmailAndPassword
 import { auth, db } from "./firebase"
 import { doc, setDoc } from "firebase/firestore"
 import AsyncStorage from "@react-native-async-storage/async-storage"
+import { GoogleAuthProvider,signInWithCredential,} from 'firebase/auth';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+WebBrowser.maybeCompleteAuthSession();
 
 // Login user
 export const login = async (email: string, password: string) => {
@@ -38,3 +43,18 @@ export const logoutUser = async () => {
   AsyncStorage.clear()
   return
 }
+
+
+export const signInWithGoogle = async () => {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: 'YOUR_GOOGLE_WEB_CLIENT_ID',   // Firebase Console එකෙන් ගන්න
+  });
+
+  if (request) {
+    const result = await promptAsync();
+    if (result?.type === 'success') {
+      const credential = GoogleAuthProvider.credential(result.params.id_token);
+      return await signInWithCredential(auth, credential);
+    }
+  }
+};
